ea4a732994305ec380ec59c87e69971510ec0da7
Fomalhaut <inso32@gmail.com>
2023-11-12 08:09:33 +0500

    Add files via upload

--- atlas.js
+++ atlas.js
@@ -1,1 +1,22 @@
+/**
+ * Write the header
+ */
+function writeHeader(baseurl){
+	
+	var gap = "&nbsp;&nbsp;&nbsp;";
+	
+	document.write(""
+		+"<center><br><h1>Atlas.Node</h1>"
+		+"<button class='solobutton' onclick='location.href=\""+baseurl+"index.html\";'>Home</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\""+baseurl+"stake.html\";'>Stake</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\""+baseurl+"questions.html\";'>Questions</button>"+gap
+		+"<br><br>"
+		+"");
+} 
 
+/**
+ * Write the footer
+ */
+function writeFooter(){
+	document.write("</center>");
+} 
