f0d9571c12d7fa03cce7babb041629b93cd9d0ee
Fomalhaut <inso32@gmail.com>
2023-12-02 08:09:51 +0500

    Update atlas.js

--- atlas.js
+++ atlas.js
@@ -3,11 +3,11 @@
  */
 function writeHeader(baseurl){
 	
-	var gap = "&nbsp;&nbsp;&nbsp;";
+	var gap = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 	
 	document.write("<center><br><h1>AURO.node</h1>"
 		+"<button class='solobutton' onclick='location.href=\""+baseurl+"index.html\";'>Home</button>"+gap
-		+"<button class='solobutton' onclick='location.href=\""+baseurl+"stake.html\";'>Stake</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\""+baseurl+"stake.html\";'>Stake CCD</button>"+gap
 		+"<button class='solobutton' onclick='location.href=\""+baseurl+"questions.html\";'>Questions</button>"+gap
 		+"<br><br>"
 		+"");
@@ -17,5 +17,7 @@ function writeHeader(baseurl){
  * Write the footer
  */
 function writeFooter(){
-	document.write("</center>");
+	document.write("<center><h2>Got a feedback ?</h2><br>We are all ears<br>atlas.node.space@gmail.com"
+	);
 } 
+
